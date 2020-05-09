fd424a1a96dee846c4909ec4511ec4caacd8eda1
Alex Hall <alex.mojaki@gmail.com>
2020-05-09 20:29:41 +0200

    Update ./make_release.sh

--- make_release.sh
+++ make_release.sh
@@ -1,19 +1,26 @@
 #!/usr/bin/env bash
 set -eux
 
-if [ -z "${1+x}" ] then
+# Ensure that there are no uncommitted changes
+# which would mess up using the git tag as a version
+[ -z "$(git status --porcelain)" ]
+
+if [ -z "${1+x}" ]
+then
     set +x
     echo Provide a version argument
     echo "${0} <major>.<minor>.<patch>"
     exit 1
 else
-    if [[ ${1} =~ ^v?([0-9]+)(\.[0-9]+)?(\.[0-9]+)?$ ]] then
+    if [[ ${1} =~ ^v?([0-9]+)(\.[0-9]+)?(\.[0-9]+)?$ ]]; then
         :
     else
         echo "Not a valid release tag."
         exit 1
     fi
 fi
+
+tox -p auto
 
 export TAG="v${1}"
 git tag "${TAG}"
